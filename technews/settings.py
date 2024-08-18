06f683d7de6a002ddb397f814c8e7473f2e0b22d
Hanane-Yh <yahyaee.hanane@gmail.com>
2024-08-19 01:15:17 +0330

    refactor: remove unnecessary code

--- technews/settings.py
+++ technews/settings.py
@@ -136,12 +136,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # configuring the location for media
 MEDIA_URL = '/media/'
